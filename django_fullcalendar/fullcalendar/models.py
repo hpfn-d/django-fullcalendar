d1a6300fb9b3100fd09b746b4a8046778957fa6a
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-12-09 10:27:38 -0200

    dunder str, smaller docstring

--- django_fullcalendar/fullcalendar/models.py
+++ django_fullcalendar/fullcalendar/models.py
@@ -1,28 +1,14 @@
-# -*- coding: utf-8 -*-
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class CalendarEvent(models.Model):
-    """The event set a record for an 
-    activity that will be scheduled at a 
-    specified date and time. 
+    """
+    The event set a record for an activity that will
+    be scheduled at a specified date and time.
     
-    It could be on a date and time 
-    to start and end, but can also be all day.
-    
-    :param title: Title of event
-    :type title: str.
-    
-    :param start: Start date of event
-    :type start: datetime.
-    
-    :param end: End date of event
-    :type end: datetime.
-    
-    :param all_day: Define event for all day
-    :type all_day: bool.
+    It could be on a date and time to start and end,
+    but can also be all day.
     """
     title = models.CharField(_('Title'), blank=True, max_length=200)
     start = models.DateTimeField(_('Start'))
@@ -33,5 +19,5 @@ class CalendarEvent(models.Model):
         verbose_name = _('Event')
         verbose_name_plural = _('Events')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
